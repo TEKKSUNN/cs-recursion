e32486d9c48d6468a653a9e627abc4a6497283e1
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-11-10 13:44:33 +0800

    build: Use ESM syntax for exporting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { fibs, fibsRec } from "./src/modules/fibonacci";
 import { mergeSort } from "./src/modules/merge-sort";
 
-module.exports = { fibs, fibsRec, mergeSort };
+export { fibs, fibsRec, mergeSort };
